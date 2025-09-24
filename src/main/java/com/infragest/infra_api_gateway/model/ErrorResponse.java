c5a56ef798cd5951ca1a8f55ce666cb51edfcfad
bunnystring <ziknaf_548@hotmail.com>
2025-09-24 07:43:02 -0500

    se agregan importaciones de loombok y ajusta properties

--- src/main/java/com/infragest/infra_api_gateway/model/ErrorResponse.java
+++ src/main/java/com/infragest/infra_api_gateway/model/ErrorResponse.java
@@ -1,11 +1,13 @@
 package com.infragest.infra_api_gateway.model;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
     private Instant timestamp;
